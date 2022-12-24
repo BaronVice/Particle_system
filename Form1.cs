81b47c5af0bdd5c16fafcbb406772f7d076d8021
BaronVice <toni.sas.228@mail.ru>
2022-12-24 12:22:52 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Particle_system
+{
+    public partial class Form1 : Form
+    {
+        // TODO: для обручей сделать класс, в котором будет метод смены цвета объекта, если тот попал в обруч
+        // у каждого обруча будет Color
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
